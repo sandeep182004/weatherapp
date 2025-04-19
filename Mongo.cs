50c3db4d92d32f7d5233fbbf18f34bd1501cd7d6
sandeep182004 <sandeep.200418@gmail.com>
2025-04-19 22:27:43 +0530

    Add files via upload

---
+++ Mongo.cs
@@ -0,0 +1,37 @@
+﻿
+using BlazorApp1.Models;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace BlazorApp1.DBConnections
+{
+    public class Mongo
+    {
+        private readonly IMongoDatabase _database;
+
+        public Mongo(string connectionString, string databaseName)
+        {
+            var client = new MongoClient(connectionString);
+            _database = client.GetDatabase(databaseName);
+        }
+
+        public IMongoCollection<User> Users
+        {
+            get
+            {
+                return _database.GetCollection<User>("Users");
+            }
+        }
+    }
+    public class Favorite
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        public string UserId { get; set; } // To associate favorites with a user
+        public string LocationName { get; set; }
+        public string LocationId { get; set; } // Optional: ID from the weather API
+        public DateTime CreatedAt { get; set; }
+    }
+}
